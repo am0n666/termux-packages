98a2a82d543080725a6f215cfc1b1ce3460c0c66
am0n666 <bethor666@gmail.com>
2019-09-11 09:56:37 +0200

    Update build.sh

--- packages/termux-api/build.sh
+++ packages/termux-api/build.sh
@@ -1,8 +1,8 @@
 TERMUX_PKG_HOMEPAGE=https://wiki.termux.com/wiki/Termux:API
-TERMUX_PKG_DESCRIPTION="Termux API commands (install also the Termux:API app)"
+TERMUX_PKG_DESCRIPTION="Termux API - static commands (install also the Termux:API app)"
 TERMUX_PKG_LICENSE="MIT"
-TERMUX_PKG_VERSION=0.46
-TERMUX_PKG_SHA256=5c4198b5a3171c1ae1dae629b102085c056d88f32b0c791eeec4d191937d96b0
-TERMUX_PKG_SRCURL=https://github.com/termux/termux-api-package/archive/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_VERSION=0.5
+TERMUX_PKG_SHA256=c4e793ab6402213efd88138486d23038e29692df3f36ddb5c590d9adc358d702
+TERMUX_PKG_SRCURL=https://github.com/am0n666/termux-api/raw/master/termux-api-package-0.46.tar.gz
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_DEPENDS="bash"
